1b8a818d317655c936378c56235e4d5bf1a054a3
sopidasri <nuunanni@gmail.com>
2024-03-28 14:24:31 +0700

    .

--- home.py
+++ home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.page_link("Home.py", label="หน้าแรก", icon="🏠")
+st.page_link("home.py", label="หน้าแรก", icon="🏠")
 
 st.page_link("pages/Statistic.py", label="การนำเสนอข้อมูลด้วยสถิติ-", icon="1️⃣")
 st.page_link("pages/Chart.py", label="การนำเสนอข้อมูลด้วยการจินตทัศน์ข้อมูล", icon="2️⃣", disabled=False)
