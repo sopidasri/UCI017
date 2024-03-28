ea37e3d666c82214df58ae13fa06c814cd4d1238
sopidasri <nuunanni@gmail.com>
2024-03-28 16:15:33 +0700

    .

--- pages/Statistic.py
+++ pages/Statistic.py
@@ -17,5 +17,13 @@ lottie_url_hello = "https://lottie.host/71e6e18c-2bab-40bd-9121-bd755b60a778/Ib8
 lottie_hello = load_lottieurl(lottie_url_hello)
 st_lottie(lottie_hello,key="hello")
 
+html_1 = """
+<div style="background-color:#52BE80;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>สถิติข้อมูล Oranges vs. Grapefruit</h5></center>
+</div>
+"""
+st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
+
 st.subheader("สถิติข้อมูล Oranges vs. Grapefruit")
 st.write(dt.head(10))
