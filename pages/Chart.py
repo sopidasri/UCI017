a0b818c2dc2a93f7c91695f9ffc6565e01845e60
sopidasri <nuunanni@gmail.com>
2024-03-28 15:40:41 +0700

    .

--- pages/Chart.py
+++ pages/Chart.py
@@ -2,6 +2,10 @@ import streamlit as st
 from streamlit_lottie import st_lottie
 from streamlit_lottie import st_lottie_spinner
 import requests
+import pandas as pd
+
+
+dt=pd.read_csv('./data/citrus.csv')
 
 def load_lottieurl(url: str):
     r = requests.get(url)
@@ -14,3 +18,13 @@ lottie_hello = load_lottieurl(lottie_url_hello)
 st_lottie(lottie_hello,key="hello")
 
 st.subheader("การนำเสนอข้อมูลด้วยการจินตทัศน์ข้อมูล")
+
+st.write("Area_Chart")
+a=dt['diameter'].mean()
+b=dt['weight'].mean()
+c=dt['red'].mean()
+d=dt['green'].mean()
+e=dt['blue'].mean()
+dxt=[a,b,c,d]
+cxx=pd.DataFrame(dxt,index=["diameter", "weight", "red","green","blue"])
+st.area_chart(cxx)
