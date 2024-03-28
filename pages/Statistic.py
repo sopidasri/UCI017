b4709b27db74f82607505541de5c18e33e1f6df9
sopidasri <nuunanni@gmail.com>
2024-03-28 14:52:07 +0700

    .

---
+++ pages/Statistic.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from streamlit_lottie import st_lottie
+from streamlit_lottie import st_lottie_spinner
+import requests
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+lottie_url_hello = "https://lottie.host/71e6e18c-2bab-40bd-9121-bd755b60a778/Ib8ewkFcIq.json"
+lottie_hello = load_lottieurl(lottie_url_hello)
+st_lottie(lottie_hello,key="hello")
