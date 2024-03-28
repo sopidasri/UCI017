e6db178f3c494576551a60864660050b3063b309
sopidasri <nuunanni@gmail.com>
2024-03-28 14:41:38 +0700

    .

--- home.py
+++ home.py
@@ -9,7 +9,7 @@ def load_lottieurl(url: str):
         return None
     return r.json()
 
-lottie_url_hello = "https://lottie.host/377a24ee-4e19-48c9-8a2f-fcada962c70a/oax9riei3m.json"
+lottie_url_hello = "https://lottie.host/880909b2-14cd-4b97-9f97-cabc9502e138/EVEflgHA6z.json"
 lottie_hello = load_lottieurl(lottie_url_hello)
 st_lottie(lottie_hello,key="hello")
 
