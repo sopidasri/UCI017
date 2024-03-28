40c4913988b086685772e6bd2ca21bda97c7b78a
sopidasri <nuunanni@gmail.com>
2024-03-28 14:46:28 +0700

    chart

--- home.py
+++ home.py
@@ -9,7 +9,7 @@ def load_lottieurl(url: str):
         return None
     return r.json()
 
-lottie_url_hello = "https://lottie.host/880909b2-14cd-4b97-9f97-cabc9502e138/EVEflgHA6z.json"
+lottie_url_hello = "https://lottie.host/71e6e18c-2bab-40bd-9121-bd755b60a778/Ib8ewkFcIq.json"
 lottie_hello = load_lottieurl(lottie_url_hello)
 st_lottie(lottie_hello,key="hello")
 
