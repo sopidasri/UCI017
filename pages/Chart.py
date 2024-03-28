825071e5b14172d841f8f2ab998343630c1f61fc
sopidasri <nuunanni@gmail.com>
2024-03-28 16:33:45 +0700

    .

--- pages/Chart.py
+++ pages/Chart.py
@@ -61,3 +61,11 @@ e=dt['blue'].mean()
 dxt=[a,b,c,d,e]
 cxx=pd.DataFrame(dxt,index=["diameter", "weight", "red","green","blue"])
 st.area_chart(cxx)
+
+labels = ['diameter', 'weight','red','green','blue']
+sizes = [35,25,15,25]
+explode = (0, 0.1,0,0) 
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+st.pyplot(fig1)
